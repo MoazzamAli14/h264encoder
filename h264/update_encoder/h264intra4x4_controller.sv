ad406174e1385f15252c17871d5ee1cccc31d2fe
MoazzamAli14 <amoazzam494@gmail.com>
2024-08-02 00:23:31 +0500

    update

--- h264/update_encoder/h264intra4x4_controller.sv
+++ h264/update_encoder/h264intra4x4_controller.sv
@@ -1,12 +1,37 @@
 module h264intra4x4_controller
 (
-    input logic clk,
-    input logic reset,
-    input logic in,
-    output logic out 
+    input logic CLK,
+    input logic READYO,
+    input logic readyod,
+    input logic FBSTROBE,
+    input logic fbpending,
+    input logic chreadyi,
+    input logic chreadyii,
+    input logic STROBEI,
+    input logic NEWLINE,
+    input logic NEWSLICE,
+    input logic outf1,
+    input logic outf,
+    input logic lvalid,
+    input logic tvalid,
+    input logic dconly,
+    input logic ,
+    input logic [5:0] statei,
+    input logic [3:0] submb, //xx,yy these also create here with submb
+    input logic [3:0] modeoi,
+    input logic [3:0] prevmode,
+    output logic , 
+    output logic , 
+    output logic , 
+    output logic , 
+    output logic , 
+    output logic , 
+    output logic [:0] , 
+    output logic [:0] , 
 );
 
-
+logic [1:0] xx,yy;
+logic [1:0] l_xx,t_yy;
 logic [4:0] c_state;
 logic [4:0] n_state;
 parameter S0    =   5'd0;
@@ -62,8 +87,8 @@ begin
 
         S3:
         begin 
-            if (statei[5:4] == yy ) n_state = S3; //submb
-            else                    n_state = S4 ; 
+            if (statei[5:4] == yy ) n_state = S3; 
+            else                    n_state = S4 ;
         end
 
         S4:
@@ -118,10 +143,10 @@ begin
         end
         S15:
         begin
-            if(xx[0] && submb != 15) //from submb
+            if(xx[0] && submb != 15) 
                 n_state = S2;
             else
-                n_state = S16;        
+                n_state = S16;
         end
         S16:
         begin 
@@ -155,50 +180,125 @@ end
 //Output logic 
 always_comb 
 begin
+    xx = {submb[2], submb[0]};
+    yy = {submb[3], submb[1]};
+    t_yy = (tvalid || yy != 0);
+    l_xx = (lvalid || xx != 0);
+    dconly_sel2 = l_xx && t_yy;
+
     case (c_state)
         S0:
         begin 
-            if(!STROBEI & NEWLINE) RST_0 = 1'b1;
-            else if (in) out = 1'b0;
-            else    out = 1'b1; 
+            if(!STROBEI & NEWLINE) rst_0 = 1'b1;
+            else                   rst_0 = 1'b0;
         end
 
         S1:
-        begin 
+        begin
+            rst_0 = 1'b0;
+            oldxx_en = 1'b1;
         end
 
         S2:
         begin 
-            out = in;
+            oldxx_en = 1'b0;
+            xxo_sel  = 1'b1;
+            topih_en = 1'b1;
         end
 
         S3:
         begin 
-            if (in) out = 1'b0;
-            else    out = 1'b1; 
+            xxo_sel  = 1'b0;
+            topih_en = 1'b0;
+            topii_en = 1'b1;
         end
 
         S4:
         begin 
-            out = in;
+            topii_en = 1'b0;     
         end
 
         S5:
-        begin 
-            if (in) out = 1'b0;
-            else    out = 1'b1; 
+        begin  
         end
 
         S6:
         begin 
-            out = in;
+            totdif_rst = 1'b1;
+            dconly_sel1 = 1'b1;
         end
 
         S7:
         begin 
-            if (in) out = 1'b0;
-            else    out = 1'b1; 
-        end          
+            totdif_rst = 1'b0;
+            dconly_sel1 = 1'b0;
+            totdif_en  = 1'b1;
+        end
+
+        S8:
+        begin 
+            totdif_en = 1'b1;
+        end
+
+        S9:
+        begin 
+            totdif_en = 1'b1;
+        end
+
+        S10:
+        begin 
+            totdif_en = 1'b1;
+        end
+
+        S11:
+        begin
+            totdif_en = 1'b0;
+            modeoi_en = 1'b1;
+            if (vtotdif <= htotdif && vtotdif <= dtotdif && !dconly) 
+                modeoi <= 8'h0;		
+            else if (htotdif <= dtotdif && !dconly) 
+                modeoi <= 8'h1;		
+            else
+                modeoi <= 8'h2;		
+        end
+
+        S12:
+        begin 
+ 
+        end
+
+        S13:
+        begin 
+        end
+        S14:
+        begin 
+
+        end
+
+        S15:
+        begin
+        end
+        S16:
+        begin 
+
+        end
+        S17:
+        begin 
+
+        end
+        S18:
+        begin 
+
+        end
+        S19:
+        begin 
+
+        end
+        S20:
+        begin 
+
+        end
+
         default: 
         begin
             out = 1'bx;
